0079a9b07b64ebc90f80bad0494876112b940309
tintun2602 <tintunn26@gmail.com>
2021-03-03 17:56:01 +0100

    changes

--- src/main/java/com/kinobaby/demo/BbyController.java
+++ src/main/java/com/kinobaby/demo/BbyController.java
@@ -1,12 +1,25 @@
 package com.kinobaby.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BbyController {
-	@GetMapping("/")
-	public String hei(String navn) {
-		return "Hei Verden" + navn;
+	private final List<Biletter> filmListe = new ArrayList<>();
+
+	@PostMapping("/lagre")
+	public void visResultat(Biletter inputFilm) {
+		filmListe.add(inputFilm);
+		
 	}
+
+	@GetMapping("/biletter")
+	public List<Biletter> hentAlle() {
+		return filmListe;
+	}
+
 }
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,62 @@
+
+// SELECTOR
+const form = document.querySelector('form'); 
+
+
+// ADD EVENTLISTENERS
+
+form.addEventListener('submit', kjøpBilett);
+form.addEventListener('reset', slettBilett);
+
+// FUNCTION
+
+function kjøpBilett() {
+
+    const kunde = {
+        film : $('#film').val(),
+        antall : $('#antall').val(), 
+        fornavn : $('#fnavn').val(),
+        etternavn : $('enavn').val(),
+        telefon : $('#telefonnr').val(), 
+        epost : $('#epost').val(),
+
+    }
+
+$.post('/lagre', kunde, function () {
+    hentAlle();
+});
+
+    $('#film').val('')
+    $('#antall').val('')
+    $('#fnavn').val('')
+    $('enavn').val('')
+    $('#telefonnr').val('')
+    $('#epost').val('')
+}
+
+function hentAlle() {
+    $.get('/biletter', function(data){
+    formaterData(data); 
+
+    });
+}
+
+const melding = document.getElementById('melding'); 
+
+function formaterData(kunder) {
+    for(const kunde of kunder) {
+        melding.innerHTML = /*html*/`
+        <div>
+        Film : ${kunde.film}, 
+        Antall : ${kunde.antall}, 
+        Fornavn : ${kunde.fornavn}, 
+        Etternavn : ${kunde.etternavn}, 
+        TelefonNr : ${kunde.telefon}, 
+        Epost : ${kunde.epost}
+        </div>
+        `
+    }
+}
+
+
+
